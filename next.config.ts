6a3c085c5c97903f5ddd73e5a253e6e95e48a0fb
BekzhanK1 <bkimadieff@gmail.com>
2025-11-16 16:46:59 +0500

    push to vps

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'standalone',
   images: {
     domains: [
       'c4dac4a814a5.ngrok-free.app',
